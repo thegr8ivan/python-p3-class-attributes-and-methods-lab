412eef4e2f63e174654757396e33a005429dba1a
thegr8ivan <ivan.kamotho@student.moringaschoool.com>
2024-10-18 19:44:31 +0300

    Initial commit with Song class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,58 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+
+        Song.add_to_artists(artist)
+        Song.add_to_genres(genre)
+
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+halo = Song("Halo", "Beyonce", "Pop")
+another_song = Song("Song 2", "Jay-Z", "Rap")
+
+print(Song.count)  # => 3
+print(Song.artists)  # => ["Jay-Z", "Beyonce"]
+print(Song.genres)  # => ["Rap", "Pop"]
+print(Song.genre_count)  # => {"Rap": 2, "Pop": 1}
+print(Song.artist_count)  # => {"Jay-Z": 2, "Beyonce": 1}
